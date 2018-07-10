a0f99d25b82c1e0d10693fb4c8741e2570567414
Jeff Mataya <jeff@jeffmataya.com>
2018-07-10 19:15:14 -0400

    First implementation of ActivityProducer
    ActivityProducer is a wrapper around a Kafka producer that structures
    messages into a format that can be used by the ActivityConsumer.

---
+++ commerce/producer.js
@@ -0,0 +1,20 @@
+const kafka = require('kafka-node');
+
+function ActivityProducer({ kafkaHost, topic }) {
+  let client = new kafka.KafkaClient({ kafkaHost });
+  this.topic = topic;
+  this.producer = new kafka.Producer(client);
+}
+
+ActivityProducer.prototype.send = (activityType, payload) => {
+  let activity = JSON.stringify({ activityType, payload });
+  let messagePayload = [{
+    topic: this.topic,
+    messages: activity
+  }];
+
+  this.producer.send(messagePayload, err => console.log(err));;
+};
+
+module.exports = ActivityProducer;
+
